4bc54a796bc7f63942b287fb3b1369847ffab90d
CodeAddictDipesh <45348267+DipesAtcodeict@users.noreply.github.com>
2020-05-23 23:22:42 +0545

    portfolio finised

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,46 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
 import { addContactRef } from '../../actions/ui';
+import { makeStyles } from '@material-ui/core';
+import Moon from './moon';
+import { Fade } from 'react-reveal';
 
 const useStyles = makeStyles({
   contact: {
     color: grey[300],
     textAlign: 'center',
+    fontSize: '1.2rem',
+    padding: '25px',
+  },
+  h1: {
+    borderBottom: '5px solid tomato',
+    display: 'inline-block',
+    padding: '25px',
+    borderRadius: '25px',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
+    marginTop: '4rem',
+  },
+  email: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginTop: '1rem',
+    marginRight: '20px',
+    marginLeft: '20px',
+  },
+  eAddress: {
+    marginTop: '20px',
+    backgroundColor: '#b07521',
+    width: 'fit-content',
+    alignSelf: 'center',
+    padding: '10px 30px',
+    fontWeight: 'bold',
+    borderRadius: '8px',
   },
 });
 
@@ -22,7 +55,21 @@ const Contact = () => {
 
   return (
     <div className={classes.contact} ref={contactRef}>
-      <h2>Contact Me At : dipeshonblack@gmail.com</h2>
+      <h1 className={classes.h1}>Contact Me</h1>
+      <div className={classes.content}>
+        <Fade left>
+          <Moon />
+        </Fade>
+        <div className={classes.email}>
+          <Fade right>
+            <h3>
+              You can keep looking at the the moon. But to contact me, you can
+              email me at :
+            </h3>
+            <span className={classes.eAddress}>dipeshonblack@gmail.com</span>
+          </Fade>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Contact/moon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './moon.css';
+
+const Moon = () => {
+  return <div className='moon'></div>;
+};
+
+export default Moon;
